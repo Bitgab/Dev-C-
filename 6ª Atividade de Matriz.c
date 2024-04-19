21a000f8f12a8cbbedbeaa8c59bcb47f63c13b02
Bitgab <134526370+Bitgab@users.noreply.github.com>
2024-04-19 11:46:36 -0300

    Update 6ª Atividade de Matriz.c

--- 6ª Atividade de Matriz.c
+++ 6ª Atividade de Matriz.c
@@ -6,52 +6,51 @@ int main(){
 setlocale(LC_ALL,"");
 	
 char nome  [3][200];
-float nota [3][2], soma;
+float nota [3][2], soma = 0;
 float media[3];
 int  i,j;
 
-// Solicitando dados ao usu·rio.
+// Solicitando dados ao usu√°rio.
 
 for(i = 0; i < 3; i++){
-	printf("Digite o nome da %d™ disciplina: ", i+1);
+	printf("Digite o nome da %d¬™ disciplina: ", i+1);
 	scanf("%s",&nome[i]);
 	
 	for (j = 0; j < 2; j++){
-		printf("Digite a %d™ nota: ",j+1);
+		printf("Digite a %d¬™ nota: ",j+1);
 		scanf("%f",&nota[i][j]);
-		
+		soma += nota[i][j];
 	}
  
-	soma = soma + nota[i][j];
-	media[i] = soma / 2;
+	media[i] = soma / 2.0;
 	soma = 0;
 
  }
  
 
-
-// Exibindo Resultado.
-
-for(i = 0; i < 3; i++){
-	printf("Nome da disciplina: %s\n", i+1,nome[i]);
-
-	for(j = 0; j < 2; j++){
-	printf("%d™ notas: %.1f \n", j+1, nota[i][j]); 
-
-	} 	  
-	printf("A mÈdia do aluno: %.2f\n",media[i]);
 	
-// If e Else.
+	// Exibindo Resultado.
+	
+	for(i = 0; i < 3; i++){
+		printf("Nome da disciplina: %s\n",nome[i]);
+	
+		for(j = 0; j < 2; j++){
+		printf("%d¬™ notas: %.1f \n", j+1, nota[i][j]); 
+	
+		} 	  
+		printf("A m√©dia do aluno: %.2f \n", media[i]);
+		
+	// If e Else.
+	
+		if (media[i] >= 7){
+			printf("Aluno Aprovado!");
+		} else if (media[i] >= 5 && media[i] < 7){
+			printf("Aluno em  Recupera√ß√£o!");
+		} else{
+			printf("Aluno Reprovado!");
+		}
 
-if (media[i] >= 7){
-	printf("Aluno Aprovado!");
-} else if (media[i] >= 5 || media[i] < 7){
-	printf("Aluno em  RecuperaÁ„o!");
-} else{
-	printf("Aluno Reprovado!");
-}
-
-}
+	}
 return 0; 
 }
 
